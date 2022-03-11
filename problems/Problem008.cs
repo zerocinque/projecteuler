8e4d03a3466900ef425de3f618cf7cc18137b777
miten <gigi.bottasso@gmail.com>
2022-03-11 15:04:42 +0100

    reformat problem 008

--- problems/Problem008.cs
+++ problems/Problem008.cs
@@ -1,22 +1,26 @@
-public class Problem008{
+public class Problem008
+{
     public string Data { get; set; }
 
-    public Problem008(){
+    public Problem008()
+    {
         // Read the text file
         var text = File.ReadAllLines(@"data/problem8matrix.txt");
 
         Data = string.Join("", text);
     }
 
-    public long Resolve(int adjacent){
+    public long Resolve(int adjacent)
+    {
         long maxValue = 0;
-        for(int i = 0; i<=Data.Length-adjacent; i++){
+        for (int i = 0; i <= Data.Length - adjacent; i++)
+        {
             long product = 1;
 
-            for(int j = 0; j<adjacent; j++)
-                product *= Data.ElementAt(i+j) -'0';
- 
-            maxValue = Math.Max(maxValue, product);                           
+            for (int j = 0; j < adjacent; j++)
+                product *= Data.ElementAt(i + j) - '0';
+
+            maxValue = Math.Max(maxValue, product);
         }
 
         return maxValue;
