9b7e3f368ea294570dd02821e8952cd0a30a2776
miten <gigi.bottasso@gmail.com>
2022-03-14 11:38:56 +0100

    fix and reformat problem 011

--- problems/Problem011.cs
+++ problems/Problem011.cs
@@ -10,27 +10,44 @@ public class Problem011
         Data = text.Select(x => (x.Split(' ').Select(Int32.Parse).ToArray())).ToArray();
     }
 
-    public long Run()
+    private long verticalProduct(int i, int j){
+        return Data[i][j] * Data[i + 1][j] * Data[i + 2][j] * Data[i + 3][j];
+    }
+
+    private long horizontalProduct(int i, int j){
+        return Data[i][j] * Data[i][j + 1] * Data[i][j + 2] * Data[i][j + 3];
+    }
+
+    private long leftDiagonalProduct(int i, int j){
+        return Data[i][j] * Data[i + 1][j - 1] * Data[i + 2][j - 2] * Data[i + 3][j - 3];
+    }
+
+    private long rightDiagonalProduct(int i, int j){
+        return Data[i][j] * Data[i + 1][j + 1] * Data[i + 2][j + 2] * Data[i + 3][j + 3];
+    }
+
+    public string Run()
     {
         long maxProduct = 0;
 
-        for (int i = 0; i < Data.Length - 4; i++)
+        for (int i = 0; i < Data.Length - 1; i++)
         {
             for (int j = 0; j < Data[i].Length - 1; j++)
             {
-                maxProduct = Math.Max(maxProduct, Data[i][j] * Data[i + 1][j] * Data[i + 2][j] * Data[i + 3][j]);
-                if (j + 3 < Data[i].Length - 1)
-                {
-                    maxProduct = Math.Max(maxProduct, Data[i][j] * Data[i][j + 1] * Data[i][j + 2] * Data[i][j + 3]);
-                    maxProduct = Math.Max(maxProduct, Data[i][j] * Data[i + 1][j + 1] * Data[i + 2][j + 2] * Data[i + 3][j + 3]);
-                }
-                if (j - 3 >= 0)
-                {
-                    maxProduct = Math.Max(maxProduct, Data[i][j] * Data[i + 1][j - 1] * Data[i + 2][j - 2] * Data[i + 3][j - 3]);
-                }
+                if(i + 3 < Data.Length - 1)
+                    maxProduct = Math.Max(maxProduct, verticalProduct(i, j));
+                
+                if(j + 3 < Data[i].Length - 1)
+                    maxProduct = Math.Max(maxProduct, horizontalProduct(i,j));
+
+                if (i + 3 < Data.Length - 1 && j - 3 >= 0)
+                    maxProduct = Math.Max(maxProduct, leftDiagonalProduct(i,j));
+
+                if (i + 3 < Data.Length - 1 && j + 3 < Data[i].Length - 1)
+                    maxProduct = Math.Max(maxProduct, rightDiagonalProduct(i, j));
             }
         }
 
-        return maxProduct;
+        return maxProduct.ToString();
     }
 }
