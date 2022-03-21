1ed975ae84856e960234137e859732538afa1e6e
miten <gigi.bottasso@gmail.com>
2022-03-21 15:52:00 +0100

    solve problem 045

--- problems/Problem045.cs
+++ problems/Problem045.cs
@@ -1,49 +1,48 @@
-public class Problem045{
+public class Problem045
+{
 
-    long triangleNumber(int n){
+    ulong triangleNumber(ulong n)
+    {
         return n * (n + 1) / 2;
     }
-    long pentagonalNumber(int n){
+    ulong pentagonalNumber(ulong n)
+    {
         return n * (3 * n - 1) / 2;
     }
-    long hexagonalNumber(int n){
+    ulong hexagonalNumber(ulong n)
+    {
         return n * (2 * n - 1);
     }
 
-    public long Run(long minimumCommonNumber, int startTriangleNumber = 0, int startPentagonalNumber = 0, int startHexagonalNumber = 0){
-        bool flag = true;
-        long commonNumber = 0;
-        
-        int i = startTriangleNumber;
-        int j = startPentagonalNumber;
-        int k = startHexagonalNumber;
+    public ulong Run(ulong minimumCommonNumber = 40755, ulong startTriangleNumber = 1, ulong startPentagonalNumber = 1, ulong startHexagonalNumber = 1)
+    {
+        ulong commonNumber = 0;
 
-        long currentTriangleNumber = triangleNumber(i);
-        long currentPentagonalNumber = pentagonalNumber(j);
-        long currentHexagonalNumber = hexagonalNumber(k);
+        ulong i = startTriangleNumber;
+        ulong j = startPentagonalNumber;
+        ulong k = startHexagonalNumber;
 
-        while(true){
+        ulong currentTriangleNumber = 0;
+        ulong currentPentagonalNumber = 0;
+        ulong currentHexagonalNumber = 0;
 
+        while (true)
+        {
             currentTriangleNumber = triangleNumber(i);
-
-            // if(currentTriangleNumber > currentPentagonalNumber)
             currentPentagonalNumber = pentagonalNumber(j);
-
-            // if(currentTriangleNumber > currentHexagonalNumber)
             currentHexagonalNumber = hexagonalNumber(k);
-            
-            long min = Math.Min(currentTriangleNumber, Math.Min(currentPentagonalNumber, currentHexagonalNumber));
 
-            if(currentTriangleNumber == currentPentagonalNumber && currentTriangleNumber == currentHexagonalNumber)
-                // commonNumber = currentTriangleNumber;
-                return currentTriangleNumber;
-            
-            if(commonNumber> minimumCommonNumber)
+            ulong min = Math.Min(currentTriangleNumber, Math.Min(currentPentagonalNumber, currentHexagonalNumber));
+
+            if (currentTriangleNumber == currentPentagonalNumber && currentTriangleNumber == currentHexagonalNumber)
+                commonNumber = currentTriangleNumber;
+
+            if (commonNumber > minimumCommonNumber)
                 return commonNumber;
 
-            if(currentTriangleNumber == min) i++;
-            if(currentPentagonalNumber == min) j++;
-            if(currentHexagonalNumber == min) k++;
+            if (currentTriangleNumber == min) i++;
+            if (currentPentagonalNumber == min) j++;
+            if (currentHexagonalNumber == min) k++;
         }
     }
 }
