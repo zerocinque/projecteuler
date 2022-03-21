76f9c208836c8c62c9505321e6b544ee592cd8e7
miten <gigi.bottasso@gmail.com>
2022-03-21 15:39:30 +0100

    solve problem 046

---
+++ problems/Problem046.cs
@@ -0,0 +1,41 @@
+using Library;
+
+public class Problem046
+{
+    public string Run()
+    {
+
+        for (ulong i = 4; true; i++)
+        {
+            if (!isOddCompositeNumber(i)) continue;
+
+            if (!goldbachConjecture(i)) return i.ToString();
+        }
+    }
+
+    private bool isOddCompositeNumber(ulong n)
+    {
+        return !n.IsPrime() && isOdd(n);
+    }
+
+    private bool isOdd(ulong n)
+    {
+        return n % 2 == 1;
+    }
+
+    private bool goldbachConjecture(ulong n)
+    {
+
+        for (ulong i = n - 2; i > 1; i--)
+        {
+            if (!i.IsPrime()) continue;
+
+            for (ulong j = 1; (2 * j * j) <= (n - i); j++)
+            {
+                if (((2 * j * j) + i) == n) return true;
+            }
+        }
+
+        return false;
+    }
+}
